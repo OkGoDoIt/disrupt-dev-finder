13aed8f38f3886ef704ff469203def6ef5385fe7
Roger <roger@betechie.com>
2014-09-06 22:03:55 +0000

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DisruptDevFinder::Application.config.secret_key_base = '4b2081b4a646693e261abe70e4a90519f789915d997a30994093245074dd2a67a7ae432716a79ae1cb42cb6e7e634e8826a68697132c4c6f242f470c66139f75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DisruptDevFinder::Application.config.session_store :cookie_store, key: '_disrupt-dev-finder_session'
